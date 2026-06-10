f22cbee53029dc3f6248b569ed7e917ef8428654
Roman <romazaniosdev@gmail.com>
2026-06-10 10:43:32 +0300

    Initial Commit

---
+++ 215TavoraHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  215TavoraHouse
+//
+//  Created by Roman on 6/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
